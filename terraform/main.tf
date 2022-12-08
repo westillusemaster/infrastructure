c2254eee631aa20a351d048533d0bc5a9601d620
itasahobby <itasahobby@gmail.com>
2022-12-08 19:22:46 +0100

    add terraform infrastructure

---
+++ terraform/main.tf
@@ -0,0 +1,33 @@
+resource "google_service_account" "default" {
+  account_id   = "service-account-id"
+  display_name = "We still use master"
+}
+
+resource "google_container_cluster" "primary" {
+  name     = "k8s-cluster"
+  location = "europe-west9"
+
+  # We can't create a cluster with no node pool defined, but we want to only use
+  # separately managed node pools. So we create the smallest possible default
+  # node pool and immediately delete it.
+  remove_default_node_pool = true
+  initial_node_count       = 1
+}
+
+resource "google_container_node_pool" "primary_preemptible_nodes" {
+  name       = "k8s-node-pool"
+  location   = "europe-west9"
+  cluster    = google_container_cluster.primary.name
+  node_count = 1
+
+  node_config {
+    preemptible  = true
+    machine_type = "e2-medium"
+
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    service_account = google_service_account.default.email
+    oauth_scopes    = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
+}
