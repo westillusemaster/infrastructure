30c5a90cc76a4b20907b1b119d26f0b7fbe1bf85
ITasahobby <47305925+itasahobby@users.noreply.github.com>
2022-12-09 20:59:20 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ resource "google_service_account" "default" {
 resource "google_container_cluster" "primary" {
   name     = "k8s-cluster"
   location = "europe-west9"
-  
+
   # We can't create a cluster with no node pool defined, but we want to only use
   # separately managed node pools. So we create the smallest possible default
   # node pool and immediately delete it.
