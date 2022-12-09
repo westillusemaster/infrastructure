7c46441e83b26c9e06c3cdf05f1bc6838997c96c
itasahobby <itasahobby@gmail.com>
2022-12-09 04:22:16 +0100

    merge terraform files

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,23 @@
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs
+provider "google" {
+  project = "swift-casing-370717"
+  region  = "europe-west9"
+}
+
+# https://www.terraform.io/language/settings/backends/gcs
+terraform {
+  backend "gcs" {
+    bucket = "westillusemaster-tf-state"
+    prefix = "terraform/state"
+  }
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"
+    }
+  }
+}
+
 resource "google_service_account" "default" {
   account_id   = "service-account-id"
   display_name = "We still use master"
